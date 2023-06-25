08e3d88c45048eb3b38e9764bab9f44ed13ccd98
C-R4Y0-N <130323486+C-R4Y0-N@users.noreply.github.com>
2023-06-25 09:34:26 -0600

    Primer commit

---
+++ divisiblenetrecuatro.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    if(n%4==0)
+    {
+        cout<<"AAAAAAAAAA!!!"<<endl;
+    }
+    else
+    {
+        cout<<"Ok"<<endl;
+    }
+}
